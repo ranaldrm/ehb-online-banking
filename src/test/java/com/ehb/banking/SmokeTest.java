9d0374855ba8e0c0750f62e3d3040b229d82767e
Ran <ranaldrm2@outlook.ocm>
2026-08-26 21:00:46 +0100

    Domain model ticket 13 complete

---
+++ src/test/java/com/ehb/banking/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.ehb.banking;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SmokeTest {
+
+    @Test
+    void junitIsWorking() {
+        assertTrue(true);
+    }
+}
