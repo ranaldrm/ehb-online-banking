56decb1f01b84ffcb6096118478be6395c031fe9
Ran <ranaldrm2@outlook.ocm>
2026-09-26 12:36:25 +0100

    s2 16

--- src/main/java/com/ehb/banking/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ehb/banking/exceptions/GlobalExceptionHandler.java
@@ -24,6 +24,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    @ExceptionHandler(NonPositiveAmountException.class)
+    public ResponseEntity<ErrorResponse> handleNonPositiveAmount(NonPositiveAmountException ex) {
+        ErrorResponse errorResponse = ErrorResponse.from(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
+    @ExceptionHandler(ExceedsBalanceException.class)
+    public ResponseEntity<ErrorResponse> handleExceedsBalance(ExceedsBalanceException ex) {
+        ErrorResponse errorResponse = ErrorResponse.from(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler(DuplicateAccountNumberException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateAccountNumber(DuplicateAccountNumberException ex) {
+        ErrorResponse errorResponse = ErrorResponse.from(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidPaymentException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidPayment(InvalidPaymentException ex) {
+        ErrorResponse errorResponse = ErrorResponse.from(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(InvalidPaymentTransitionException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidPaymentTransition(InvalidPaymentTransitionException ex) {
+        ErrorResponse errorResponse = ErrorResponse.from(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
 }
